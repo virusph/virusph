128e0314affd86010fcc8ff24f4f42b88e26019d
Miguel Aquino <jcaquino0945@gmail.com>
2020-05-09 23:21:22 +0800

    1st commit: imported from org source

--- home.php
+++ home.php
@@ -1,12 +1,13 @@
 <?=template_header('Home')?>
 <div class="container-fluid">
   <div class="row" style="margin-top: 4em; height:auto;">
-    <div class="col-xl-6 col-sm-12">
+    <div class="col-xl-6">
     <div class="bingwidget" data-type="covid19_modules" data-modules='[{"_type": "stats", "location-id":"/Philippines"},{"_type": "stats", "location-id":"/"},  {"_type": "map", "location-id":"/Philippines"},  {"_type": "trends", "location-id":"/Philippines"}]'></div>
   <script src="//www.bing.com/widget/bootstrap.answer.js" async=""></script>
+  <!-- start feedwind code --> <script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" preloader-text="Loading" data-fw-param="129413/"></script> <!-- end feedwind code -->
     </div>
-    <div class="col-xl-6 col-sm-12">
-    Test
+    <div class="col-xl-6">
+   
   </div>
 </div>
 <?=template_footer()?>
--- templates.php
+++ templates.php
@@ -20,11 +20,17 @@ function template_header($title) {
     <html>
         <head>
             <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>$title</title>
             <link rel="stylesheet" href="css/bootstrap.css">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
             <script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>
 <link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet' />
+<style>
+iframe {
+height: 1450px;
+}
+</style>
             </head>
 <body>
 <header>
@@ -58,11 +64,6 @@ EOT;
 
 function template_footer() {
     echo <<<EOT
-    <footer class="footer mt-auto py-3" style="position:fixed; bottom:0;width:100%;">
-    <div class="container">
-      <span class="text-muted">VirusPh 2020</span>
-    </div>
-  </footer>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
